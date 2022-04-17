b06fdd310ae470535403ea5ad01596f424cc2fbf
Khaled3bzaher <62258636+Khaled3bzaher@users.noreply.github.com>
2022-04-18 01:30:21 +0200

    Pre-Processing Data

--- Pre_processing.py
+++ Pre_processing.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 import numpy as np
 def Feature_Encoder(X,cols):
@@ -12,4 +13,5 @@ def featureScaling(X,a,b):
     Normalized_X=np.zeros((X.shape[0],X.shape[1]))
     for i in range(X.shape[1]):
         Normalized_X[:,i]=((X[:,i]-min(X[:,i]))/(max(X[:,i])-min(X[:,i])))*(b-a)+a
-    return Normalized_X
+    DataFrameReturned=pd.DataFrame(Normalized_X,columns=['airline','ch_code','num_code','dep_time','time_taken','stop','arr_time','type','source','destination','day','month'])
+    return DataFrameReturned
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import numpy as np
 import pandas as pd
 import seaborn as sns
@@ -19,13 +21,65 @@ X=data.iloc[:,0:10] #Features
 #OutPut
 Y=data['price']
 
+
 #PreProcessing
+#Stop Column
 X['stop'] = (X["stop"].str.split("-", n = 1, expand = True))[0]
 X['stop']=X['stop'].replace('non','0')
 X['stop']=X['stop'].replace('2+','2')
+X['stop']=pd.to_numeric(X['stop'])
+#dep_time Column
+X['dep_time']=X['dep_time'].str.replace(':','.')
+X['dep_time']=pd.to_numeric(X['dep_time'])
+#arr_time Column
+X['arr_time']=X['arr_time'].str.replace(':','.')
+X['arr_time']=pd.to_numeric(X['arr_time'])
+#route Column
+new=X["route"].str.split(", ")
+source = []
+destination = []
+for x in new:
+    s = str(x[0]).split(": ")
+    source.append(s[1])
+    d = str(x[1]).split(": ")
+    d = str(d[1]).split("}")
+    destination.append(d[0])
+X = X.drop(['route'], axis = 1)
+X['source'] = source
+X['destination']=destination
 
+#timeTaken Column
+X['time_taken']=X['time_taken'].str.replace('h m','')
+X['time_taken']=X['time_taken'].str.replace('h ','.')
+X['time_taken']=X['time_taken'].str.replace('m','')
+X['time_taken']=pd.to_numeric(X['time_taken'])
+
+#Date Column
+newDate = X['date']
+columnDate = []
+day=[]
+month=[]
+for i in range(len(data)):
+    if newDate[i][-5] == "-":
+        columnDate.append(datetime.strptime(newDate[i], '%d-%m-%Y'))
+    elif newDate[i][-5] == "/":
+        columnDate.append(datetime.strptime(newDate[i], '%d/%m/%Y'))
+    day.append(columnDate[i].day)
+    month.append(columnDate[i].month)
+
+X = X.drop(['date'], axis = 1)
+X['day']=day
+X['month']=month
+
+X['day']=pd.to_numeric(X['day'])
+X['month']=pd.to_numeric(X['month'])
 
 #Encoding
-cols=('airline','arr_time','ch_code','type','dep_time')
+cols=('airline','ch_code','type','source','destination')
 X=Feature_Encoder(X,cols)
+
+#Feature Scaling
+X=featureScaling(X,0,10) #Look At Meeeee Agaaaaiiiiinnnnnnnnn
+
+print(X.head())
 
