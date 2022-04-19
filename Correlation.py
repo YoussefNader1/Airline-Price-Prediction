3f72611389418ae954bce900333d85fa16bcd0bb
el a5tabot <youssef20191700793@cis.asu.edu.eg>
2022-04-20 00:55:10 +0200

    Added ploynomial regression teq. , added heatmap for visualization for correlation
    polynomial of oreder 9 note that higher than 9 will cause overfitting
    Co-authored-by: abanoubgamal33 <abanoubgamal33@gmail.com>
    Co-authored-by: Khaled3bzaher <khaled3bzaher@gmail.com>
    Co-authored-by: KerolosNabil7 <Kerolosnabil744@gmail.com>

--- Correlation.py
+++ Correlation.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+
 
 def correlation(X, Y):
 
@@ -6,5 +9,9 @@ def correlation(X, Y):
     result_corr = result.corr()
     # return only features with correlation more than 0.5
     top_feature = result_corr.index[abs(result_corr['price']) > 0.2]
+    plt.subplots(figsize=(12, 8))
+    top_corr = result[top_feature].corr()
+    sns.heatmap(top_corr, annot=True)
+    plt.show()
     top_feature = top_feature.delete(-1)
     return top_feature
--- main.py
+++ main.py
@@ -55,9 +55,25 @@ print("Enter 1 for multiple regression model\nEnter 2 for polynomial regression"
 choice = int(input("Choose your model: "))
 if choice == 1:
 
+
     cls = linear_model.LinearRegression()
     cls.fit(x_train,y_train)
     Prediction = cls.predict(x_test)
+
+    # plt.style.use('default')
+    # plt.style.use('ggplot')
+    #
+    # fig, ax = plt.subplots(figsize=(7, 3.5))
+    #
+    # ax.plot(x_train, y_train, color='k', label='Regression model')
+    # ax.scatter(x_train, Prediction, edgecolor='k', facecolor='grey', alpha=0.7, label='Sample data')
+    # ax.set_ylabel('Gas production (Mcf/day)', fontsize=14)
+    # ax.set_xlabel('Porosity (%)', fontsize=14)
+    # ax.legend(facecolor='white', fontsize=11)
+    # ax.text(0.55, 0.15, '$y = %.2f x_1 - %.2f $' % (cls.coef_[0], abs(cls.intercept_)), fontsize=17,
+    #         transform=ax.transAxes)
+    #
+    # fig.tight_layout()
 
     print(cls.score(x_test, y_test))
     print('Co-efficient of linear regression', cls.coef_)
@@ -69,3 +85,31 @@ if choice == 1:
 
     print("The true price value " + str(true_price_value))
     print("The predicted price value " + str(predicted_price_value))
+
+elif choice == 2:
+    poly_features = PolynomialFeatures(degree=9)
+
+    # transforms the existing features to higher degree features.
+    X_train_poly = poly_features.fit_transform(x_train)
+
+    # fit the transformed features to Linear Regression
+    poly_model = linear_model.LinearRegression()
+    poly_model.fit(X_train_poly, y_train)
+
+    # predicting on training data-set
+    y_train_predicted = poly_model.predict(X_train_poly)
+    ypred = poly_model.predict(poly_features.transform(x_test))
+
+    # predicting on test data-set
+    prediction = poly_model.predict(poly_features.fit_transform(x_test))
+
+    # print(poly_model.score(x_test, y_test))
+
+    print('Co-efficient of linear regression', poly_model.coef_)
+    print('Intercept of linear regression model', poly_model.intercept_)
+    print('Mean Square Error', metrics.mean_squared_error(y_test, prediction))
+
+    true_player_value = np.asarray(y_test)[0]
+    predicted_player_value = prediction[0]
+    print('True value for the first player in the test set in millions is : ' + str(true_player_value))
+    print('Predicted value for the first player in the test set in millions is : ' + str(predicted_player_value))
